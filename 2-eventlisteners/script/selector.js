5c2f2b247c18681dd8237116072b02953d037a02
Sewilius Berg <code@incomplete.se>
2022-07-30 16:48:56 +0200

    Finished exercise 2

--- 2-eventlisteners/script/selector.js
+++ 2-eventlisteners/script/selector.js
@@ -1,26 +1,6 @@
-// Eventlistener
-
-let button = document.getElementsByTagName('button');
-
-function popUp() {
-    alert("Hello nr 2");
-}
-
-button[1].addEventListener("click", popUp);
-
-// ------------------
-let div = document.getElementsByTagName('div');
-
-function changeBackColor(){
-    div[3].style.backgroundColor = "rgb(255, 255, 255)";
-}
-
-div[3].addEventListener("mouseover", changeBackColor);
-
-function remove(){
-    div[3].removeEventListener("mouseover", changeBackColor);
-}
-
-let btn = document.getElementsByClassName('trams');
-console.log(btn);
-btn.addEventListener("click", remove);
+/*  Eventlistener
+    -----------------------
+    1) En inline eventlistener (onClick) med alert, prompt & confimr
+    2) En extern eventlistener (click)
+    3) En extern event (inte click) på annat än knapp
+*/
